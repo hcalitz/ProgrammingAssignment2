3ebc0e7c570de185eef3b599c27f952595d934ad
hcalitz <hcalitz@users.noreply.github.com>
2014-11-22 23:55:28 +1300

    Update cachematrix.R
    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The inverse of a matrix is calculated and stored as a list
+## in a separate memory environment.
+## Subsequent calls with the same list as input will use the 
+## cached value instead of doing the calculation
 
-## Write a short comment describing this function
+## Calculate and store the inverse of a matrix to a list
+## Input : a square matrix
+## Output: a list with set, get, setinv and getinv
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Set the list with matrix and NULL inverse
+    m <- NULL
+    set <- function(y) {
+        # set matrix
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x 
+    setinv <- function(solve) m <<- solve 
+    getinv <- function() m
+    #return list
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## Calculate inverse of matrix if new or recall if exists
+## Input : list output from makeCacheMatrix
+## Output: Inverse of matrix with message if cached matrix is used 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    
+    # Does the inverse exists?
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    # Inverse does not exists, calculate it
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
